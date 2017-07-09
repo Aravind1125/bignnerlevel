a18999d6095a429b49d1ad0efa1a8c3840b0a860
Aravind1125 <aravindkumar_ravi@srmuniv.edu.in>
2017-07-09 11:46:17 +0530

    Add files via upload

---
+++ class my prog.java
@@ -0,0 +1,17 @@
+
+class MyProgram
+{
+public static void main(String args[])
+{
+if(6>3) 
+{
+System.out.println('6 is greater than 3');
+}
+if(6 < 4) 
+{
+System.out.println('4 is greater than 6');
+//the above line is false therefore will not print
+}
+}
+}
+Output: 6 is greater than 3
